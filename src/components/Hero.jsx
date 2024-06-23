0ea53b8c5b3356c55f4342c35f84c17f7febaec8
Albert Nabil <136574777+albert273@users.noreply.github.com>
2024-06-23 18:03:05 +0300

    Update Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             web developer 3D visuals, <br className='sm:block hidden' />
-            Front-End and UI & UX Designer
+            Full-Stack and UI & UX Designer
           </p>
         </div>
       </div>
